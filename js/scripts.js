b08959c388ed4a2c2300edc34188bfefaee33e49
thainadesousa <thaina.pereira4@aluno.pb.gov.br>
2024-06-27 12:07:44 -0300

    Iniciando JS no projeto
    Adicionei o id btn no "acessar conta", ajeitei o o link script no index.html, criei a função do btn e a validação do form

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('form');
+const nomeInput = document.getElementsByClassName('entrada-de-nome');
+const senhaInput = document.getElementsByClassName('entrada-de-senha');
+const url = '../index.html'
+const btn = document.getElementById('btn');
+
+// Função btn | add um evento para o btn
+function openInNewTab(url){
+    const win = window.open(url, '_blank')
+    win.focus
+}
+
+btn.addEventListener('click', () => {
+    openInNewTab(url)
+})
+
+// Pegando os valores do input
+const nomeValue = nomeInput.value;
+const senhaValue = senhaInput.value;
+
+// Add um evento para o form | validação de formulário
+form.addEventListener('submit', (e) => {
+    
+    // Para que a pgn não fique atualizando ao clicar
+    e.preventDefault
+
+    if(nomeValue == '') {
+        alert('Preencha o campo com um nome de usuário');
+    }
+    if(senhaValue == '') {
+        alert('Preencha o campo com uma senha');
+    }
+    else {
+        openInNewTab(url);
+    }
+})
