80da89a67d048748fbe4ba62e2580072d26b0c8b
thainadesousa <thaina.pereira4@aluno.pb.gov.br>
2024-07-01 18:11:30 -0300

    Atualizando login e cadastro

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 const form = document.querySelector('#form');
 const nomeInput = document.getElementsByClassName('entrada-de-nome');
 const senhaInput = document.getElementsByClassName('entrada-de-senha');
+const nomeLogin = document.getElementsByClassName('entrada-do-nome-login');
+const senhaLogin = document.getElementsByClassName('entrada-da-senha-login');
+
 const url = '../index.html'
 const btn = document.getElementById('btn');
 
@@ -17,6 +20,15 @@ btn.addEventListener('click', () => {
 // Pegando os valores do input
 const nomeValue = nomeInput.value;
 const senhaValue = senhaInput.value;
+const nomeLoginValue = nomeLogin.value;
+const senhaLoginvalue = senhaLogin.value;
+
+logins = [
+    {
+        nome: "Valter",
+        senha: "123"
+    }
+]
 
 // Add um evento para o form | validação de formulário
 form.addEventListener('submit', (e) => {
@@ -24,26 +36,29 @@ form.addEventListener('submit', (e) => {
     // Para que a pgn não fique atualizando ao clicar
     e.preventDefault
 
-    if(nomeValue == '') {
-        alert('Preencha o campo com um nome de usuário');
+    class Login {
+        constructor(nome,senha) {
+            this.nome;
+            this.senha
+        }
     }
-    if(senhaValue == '') {
-        alert('Preencha o campo com uma senha');
+
+    const usuarios = [
+        {
+            nome: "Valter",
+            senha: 123
+        }
+    ]
+
+    if(nomeValue.lenght > 0 && senhaValue.lenght > 0){
+       usuarios.push(new Login(nomeValue, senhaValue))
+    } else{
+        alert('Preencha os campos de cadastro')
     }
-    else {
-        openInNewTab(url);
+    if(nomeLogin.length > 0 && senhaLogin.length > 0) {
+       openInNewTab
+    } else {
+        alert('Preencha os campos de login')
     }
 })
 
-//add ao carrinho
-
-const addCar = document.getElementsByClassName('add-carrinho');
-for (const i = 0; i < addCar.length; i++){
-    addCar[i].addEventListener('click', addProduto)
-}
-
-function addProduto(event) {
-    const botao = event.target;
-    const produtoInfos = botao.parentElement.parentElement;
-    console.log(produtoInfos)
-}
