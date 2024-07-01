451a35d40957a04341d0c46b95e46dd0f3f6a149
richarlysonayala <richarlisonayala@gmail.com>
2024-07-01 19:05:55 -0300

    adicionado o hidden

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const iconeCarrinho = document.querySelector('.fa-cart-shopping');
 const listCar = document.querySelector('#bar-car');
+const body = document.querySelector("body")
 
 iconeCarrinho.addEventListener('click', () => {
     listCar.style.display = "flex";
+    body.style.overflow = "hidden"
 })
