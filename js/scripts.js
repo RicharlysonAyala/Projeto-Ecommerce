f23234ae468992f41ce2476ed6cc3d4c819a21b6
thainadesousa <thaina.pereira4@aluno.pb.gov.br>
2024-07-01 17:17:12 -0300

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const form = document.querySelector('form');
+const form = document.querySelector('#form');
 const nomeInput = document.getElementsByClassName('entrada-de-nome');
 const senhaInput = document.getElementsByClassName('entrada-de-senha');
 const url = '../index.html'
@@ -20,7 +20,7 @@ const senhaValue = senhaInput.value;
 
 // Add um evento para o form | validação de formulário
 form.addEventListener('submit', (e) => {
-    
+
     // Para que a pgn não fique atualizando ao clicar
     e.preventDefault
 
@@ -34,3 +34,16 @@ form.addEventListener('submit', (e) => {
         openInNewTab(url);
     }
 })
+
+//add ao carrinho
+
+const addCar = document.getElementsByClassName('add-carrinho');
+for (const i = 0; i < addCar.length; i++){
+    addCar[i].addEventListener('click', addProduto)
+}
+
+function addProduto(event) {
+    const botao = event.target;
+    const produtoInfos = botao.parentElement.parentElement;
+    console.log(produtoInfos)
+}
