da2437697513f8cdb617e1db89624d00d77c1817
richarlysonayala <richarlisonayala@gmail.com>
2024-07-01 18:47:06 -0300

    ajuste

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const form = document.querySelector('#form');
-const nomeInput = document.getElementsByClassName('.entrada-de-nome');
-const senhaInput = document.getElementsByClassName('.entrada-de-senha');
-const nomeLogin = document.getElementsByClassName('.entrada-do-nome-login');
-const senhaLogin = document.getElementsByClassName('.entrada-da-senha-login');
-
-const url = '../index.html'
-const btnCadastro = document.getElementById('btn');
-const
-=======
 const formLogin = document.querySelector('#form-login');
 const formCadastro = document.querySelector('#form-cadastro');
 const nomeInput = document.querySelectory('.entrada-do-nome-cadastro');
@@ -19,7 +8,6 @@ const senhaLogin = document.querySelectory('.entrada-da-senha-login');
 const url = '../index.html'
 const btnCadastro = document.querySelector('#cadastro-btn');
 const btnLogin = document.querySelector('#login-btn');
->>>>>>> 889854faed99112abd19eb1969c4862565174c3e
 
 // Função btn | add um evento para o btn
 function openInNewTab(url){
@@ -33,8 +21,6 @@ const senhaValue = senhaInput.value;
 const nomeLoginValue = nomeLogin.value;
 const senhaLoginvalue = senhaLogin.value;
 
-<<<<<<< HEAD
-=======
 const logins = [
     {
         nome: "Valter",
@@ -42,7 +28,6 @@ const logins = [
     }
 ]
 
->>>>>>> 889854faed99112abd19eb1969c4862565174c3e
 // Add um evento para o form | validação de formulário
 formLogin.addEventListener('submit', (e) => {
 
@@ -68,13 +53,9 @@ formLogin.addEventListener('submit', (e) => {
     } else{
         alert('Preencha os campos de cadastro')
     }
-<<<<<<< HEAD
-    if(nomeLogin.length > 0 && senhaLogin.length > 0) {
-=======
     if(usuarios.map((usuarios => {
         nomeLogin == usuarios[0] && senhaLogin == usuarios[1];
     }))) {
->>>>>>> 889854faed99112abd19eb1969c4862565174c3e
        openInNewTab()
     } else {
         alert('Preencha os campos de login')
