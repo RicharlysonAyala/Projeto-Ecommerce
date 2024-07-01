a7de06412823b76c85d9894aacae44e765fc5509
thainadesousa <thaina.pereira4@aluno.pb.gov.br>
2024-07-01 18:49:16 -0300

    Corrigindo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 const iconeCarrinho = document.querySelector('.fa-cart-shopping');
 const listCar = document.querySelector('#bar-car');
 
-listCar.style.display = "flex";
+
+iconeCarrinho.addEventListener('click', (e) => {
+    listCar.style.display = "flex";
+})
