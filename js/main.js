757a710ed23bfd7657fb28b4738edb1f003105b2
thainadesousa <thaina.pereira4@aluno.pb.gov.br>
2024-07-01 19:11:42 -0300

    função para fechar carrinho

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 const iconeCarrinho = document.querySelector('.fa-cart-shopping');
 const listCar = document.querySelector('#bar-car');
-const body = document.querySelector("body")
+const body = document.querySelector("body");
+const fechar = document.querySelector('#fechar')
 
 iconeCarrinho.addEventListener('click', () => {
     listCar.style.display = "flex";
     body.style.overflow = "hidden"
+})
+
+fechar.addEventListener('click', () => {
+    listCar.style.display = "none";
+    body.style.overflow = "none"
 })
