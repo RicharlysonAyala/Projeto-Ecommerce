45993bcead60f421e383f1d23127383735f00ee2
richarlysonayala <richarlisonayala@gmail.com>
2024-07-01 23:40:21 -0300

    Animação do carrinho de compras (estilização e elementos

--- js/main.js
+++ js/main.js
@@ -2,13 +2,20 @@ const iconeCarrinho = document.querySelector('.fa-cart-shopping');
 const listCar = document.querySelector('#bar-car');
 const body = document.querySelector("body");
 const fechar = document.querySelector('#fechar')
+const corPreta = document.querySelector("#cor-preta")
 
 iconeCarrinho.addEventListener('click', () => {
-    listCar.style.display = "flex";
+    listCar.classList.remove("fechar")
+    listCar.classList.add("abrir")
     body.style.overflow = "hidden"
+    listCar.style.display = "flex"
 })
 
 fechar.addEventListener('click', () => {
-    listCar.style.display = "none";
+    listCar.classList.remove("abrir")
+    listCar.classList.add("fechar")
     body.style.overflow = "scroll"
+    setTimeout(() => {
+        listCar.style.display = "none"
+    }, 500);
 })
