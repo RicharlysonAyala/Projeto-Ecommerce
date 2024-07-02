dcd570443ec3e6aee4b1f310d70f63c9288dd85e
richarlysonayala <richarlisonayala@gmail.com>
2024-07-02 07:57:46 -0300

    Adicionando funcionalidade de pesquisa

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
+// Produtos
+const produtos2 = document.querySelectorAll(".produto");
+
+
+// Seleção de elementos
 const iconeCarrinho = document.querySelector('.fa-cart-shopping');
 const listCar = document.querySelector('#bar-car');
 const body = document.querySelector("body");
 const fechar = document.querySelector('#fechar')
 const corPreta = document.querySelector("#cor-preta")
 
+const barraDePesquisa = document.querySelector("#pesquisa")
+const lupa = document.querySelector("#lupa")
+
+let valueInput;
+
+console.log(lupa)
+
+
+// Evento de abrir a barra
 iconeCarrinho.addEventListener('click', () => {
     listCar.classList.remove("fechar")
     listCar.classList.add("abrir")
@@ -11,6 +25,7 @@ iconeCarrinho.addEventListener('click', () => {
     listCar.style.display = "flex"
 })
 
+// Evento de fechar a barra
 fechar.addEventListener('click', () => {
     listCar.classList.remove("abrir")
     listCar.classList.add("fechar")
@@ -19,3 +34,36 @@ fechar.addEventListener('click', () => {
         listCar.style.display = "none"
     }, 459);
 })
+
+
+// Procurar produtos
+const procurarProdutos = (search) => {
+    produtos2.forEach((produto) => {
+        const nomeProduto = produto.querySelector("h3").innerText.toLowerCase();
+
+        const nomePesquisa = search.toLowerCase();
+
+        produto.style.display = "inline"
+
+        if (!nomeProduto.includes(nomePesquisa)) {
+            produto.style.display = "none"
+        }
+    });
+};
+
+barraDePesquisa.addEventListener("keyup", (e) => {
+    const search = e.target.value;
+
+    valueInput = e.target.value;
+
+    procurarProdutos(search);
+
+});
+
+lupa.addEventListener("click", () => {
+    if (valueInput === undefined) {
+        return;
+    };
+    
+    procurarProdutos(valueInput);
+});
