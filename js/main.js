b6d3e4a4390dc65b73a35d17d342be84d8ed578f
thainadesousa <thaina.pereira4@aluno.pb.gov.br>
2024-07-03 08:19:16 -0300

    ajuste

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Close/Open carrinho
+
 const iconeCarrinho = document.querySelector('.fa-cart-shopping');
 const listCar = document.querySelector('#bar-car');
 const body = document.querySelector("body");
@@ -11,4 +13,14 @@ iconeCarrinho.addEventListener('click', () => {
 fechar.addEventListener('click', () => {
     listCar.style.display = "none";
     body.style.overflow = "scroll"
+})
+
+// Lista de Desejos
+ 
+const iconeDesejo = document.querySelector('.fa-heart coracao-desejo');
+const listaDesejo = document.querySelector('.agrupamento-desejo');
+const produto = document.querySelector('.produto')
+
+iconeDesejo.addEventListener('click', () => {
+    
 })
